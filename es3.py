bfa5c2f79ca7b71e4ff0cb2595df9b481d0dd6cf
PetrocelliGian02 <gianmarcopetrocelli27@gmail.com>
2024-06-19 15:19:17 +0200

    Add files via upload
    es3

--- es3.py
+++ es3.py
@@ -4,3 +4,10 @@ if Età > EtàGiusta:
     print("puoi vedere questo film")
 else:
     print("Mi dispiace, non puoi vedere questo film")    
+
+EtàGiusta2= 18
+Età2= int(input("scrivi la tua età"))
+if Età2 > EtàGiusta2:
+    print("puoi vedere questo film")
+else:
+    print("Mi dispiace, non puoi vedere questo film")        
