3dcfcacc4f26c5780d2a7b4f38030613589a2aca
PetrocelliGian02 <gianmarcopetrocelli27@gmail.com>
2024-06-19 16:55:26 +0200

    Add files via upload
    es4

--- es4.py
+++ es4.py
@@ -10,3 +10,20 @@ if selezione == "si":
           
           selezione2 = input("vuoi vedere pallone?")
           if selezione2.lower() == "si":
+               print("operazione effettuata")
+               
+        
+          selezione3 = input("vuoi vedere gelato? ")
+          if selezione3.lower() == "si":
+               print("operazione effettuata")
+               
+               
+          
+          selezione4 = input("vuoi vedere telecomando?")
+          if selezione4.lower() == "si":
+               print("operazione effettuata")
+               
+          selezioneFinale = input("vuoi uscire?")
+          if selezioneFinale.lower() == "si":
+               print("operazione effettuata")
+               break
