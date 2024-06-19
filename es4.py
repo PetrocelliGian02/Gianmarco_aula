56526d397683751ac17460470c9f980b7ac2a55c
PetrocelliGian02 <gianmarcopetrocelli27@gmail.com>
2024-06-19 16:35:01 +0200

    Add files via upload
    esercizio 4

---
+++ es4.py
@@ -0,0 +1,12 @@
+controllo = True
+
+selezione = input("ciao Gianmarco cominciamo...")
+
+lista_prodotti = ["pallone", "gelato", "telecomando"]
+
+if selezione == "si":
+     
+     while controllo:
+          
+          selezione2 = input("vuoi vedere pallone?")
+          if selezione2.lower() == "si":
